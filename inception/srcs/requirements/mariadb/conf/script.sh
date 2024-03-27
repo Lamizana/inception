64bb4e7c78050d9bcd2a40b0cad055c4127afdcc
Alex Lamizana <alamizan@3C5.42angouleme.fr>
2024-03-27 14:27:21 +0100

    wordpress et mariadb ok

--- inception/srcs/requirements/mariadb/conf/script.sh
+++ inception/srcs/requirements/mariadb/conf/script.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "lancement script mariadb"
 service mariadb start;
-service mariadb status;
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
 mysql -u root -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+#mysql -u root -e "ALTER USER '${SQL_USER}' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
 mysql -u root -e "FLUSH PRIVILEGES;"
 
 mysqladmin -u root shutdown
+
 exec mysqld_safe
